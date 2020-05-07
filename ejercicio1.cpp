24a3cae8ae9635e4f73d0d42f09674a1d0c6871b
RPonce51 <00143220@uca.edu.sv>
2020-05-06 18:48:46 -0600

    ejercicios

---
+++ ejercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  int num1, num2, num3;
+
+    float x;
+
+    cout<< "Ingrese su primer numero";
+  cin>> num1;
+    cout<< "Ingrese su segundo numero";
+  cin>> num2;
+    cout<< "Ingrese su tercer numero";
+  cin>> num3;
+
+    x = (num1+num2+num3)/(3);
+    cout<< x;
+    return 0;
+}
